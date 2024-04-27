e576d1dffa3c2f8360a4c944c5cd19c69dd60a89
Alexander Nikolaev <aanikolaev_10@edu.hse.ru>
2024-04-27 18:57:26 +0300

    hash implementation

--- hash.c
+++ hash.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() {
-    
+
+size_t hash(char *name, char *id) {
+	char *res = NULL;
+//	int sum = 0;
+	size_t result = 533436788;
+	size_t size = strlen(name) + strlen(id) + 1;
+
+    res = (char *)malloc(size);
+	memset(res, 0, size);
+	strcpy(res, name);
+	strcat(res, id);
+/*
+	for(int i = 0; i < strlen(name); ++i) {
+		sum ^= name[i];
+	}
+*/
+	
+	printf("res str = %s\n", res);
+
+	for(int i = 0; i < strlen(res); ++i) {
+		result = (result << 2) * res[i] ^ (result >> 1);
+	}
+
+	free(res);
+
+	return result;
+}
+
+
+int main(int argc, char **argv) {
+	printf("res = %zu\n", hash(argv[1], argv[2]));
+
+	return 0;
 }
